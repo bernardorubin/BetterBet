da3b2d35745040166b3e0cbe3a15b66d8e471f39
bernulander <bernulander@gmail.com>
2017-03-26 01:35:32 -0700

    edited chartkick colors

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,4 +1,4 @@
 Chartkick.options = {
   height: "400px",
-  colors: ["#D9B76B", "#98D9E8", "#50A266", "#AF413B", "#D9C46B"],
+  colors: ["#D9B76B", "#98D9E8", "#50A266", "#AF413B", "#881F19", "#85C88D", "#535F94", "#E56218"],
 }
